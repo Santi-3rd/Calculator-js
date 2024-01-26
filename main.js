adbbf9afc1d25998c7e43aba0fdcdfb290ecdd8d
Guillermo Santiago <g.santiago3rd@gmail.com>
2024-01-26 12:55:33 -0700

    started building functions

---
+++ main.js
@@ -0,0 +1,25 @@
+
+let num1 = 0;
+let operator = '';
+let num2 = 0;
+
+
+function add() {
+    return num1 + num2
+}
+
+function subtract() {
+    return num1 - num2
+}
+
+function multiply() {
+    return num1 * num2
+}
+
+function divide() {
+    return num1 / num2
+}
+
+function operate() {
+    
+}

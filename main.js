eb2e1280072347d2c67bd99bffa4a159d6432915
Guillermo Santiago <g.santiago3rd@gmail.com>
2024-01-29 09:31:16 -0700

    built functioning initial display and clear button

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 
-let display = document.querySelector('#display');
-    display.textContent = '0'
+let startingDisplay = document.querySelector('#display');
+
+let newDisplay = '0'
 
 let num1 = 2;
 let operator = '';
 let num2 = 2;
 
-function displayHandler() {
-    
-}
+let digits = document.querySelectorAll(".digits button");
+digits.forEach(button => {
+    button.onclick = function() {
+        if (newDisplay === '0' && button.innerText !== '0') {
+            newDisplay = button.innerText;
+        } else {
+            newDisplay += button.innerText;
+        }
+        startingDisplay.innerText = newDisplay
+    };
+});
+
+let operators = document.querySelectorAll(".operators button");
+operators.forEach(button => {
+    button.onclick = function() {
+        operator = button.innerText
+    }
+})
 
 function add() {
     return num1 + num2
@@ -26,10 +42,16 @@ function divide() {
     return num1 / num2
 }
 
-console.log(add())
-console.log(subtract())
-console.log(multiply())
-console.log(divide())
+clear = document.querySelector('#clear');
+clear.onclick = function() {
+    startingDisplay.textContent = '0'
+    newDisplay = '0'
+}
+
+// console.log(add())
+// console.log(subtract())
+// console.log(multiply())
+// console.log(divide())
 
 
 function operate(num1, operator, num2) {

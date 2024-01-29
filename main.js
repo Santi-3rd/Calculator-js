15d45999efcf05470e550722bf18031a015e8533
Guillermo Santiago <g.santiago3rd@gmail.com>
2024-01-29 12:11:45 -0700

    All operators working

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ let newNum = '0'
 let num1;
 let operator = '';
 let total = 0;
-// let num2;
 
 let digits = document.querySelectorAll(".digits button");
 digits.forEach(button => {
@@ -24,41 +23,58 @@ digits.forEach(button => {
 let operators = document.querySelectorAll(".operators button");
 operators.forEach(button => {
     button.onclick = function() {
-        num1 = newNum
+        if (operator == '') {
+            num1 = newNum
+        }
         operator = button.innerText;
         newNum = '0'
     }
 });
-
 
 function add() {
     return Number(num1) + Number(newNum)
 }
 
 function subtract() {
-    return num1 - num2
+    return Number(num1) - Number(newNum)
 }
 
 function multiply() {
-    return num1 * num2
+    return Number(num1) * Number(newNum)
 }
 
 function divide() {
-    return num1 / num2
+    return Number(num1) / Number(newNum)
 }
 
 clear = document.querySelector('#clear');
 clear.onclick = function() {
     startingNum.textContent = '0'
     newNum = '0'
+    operator = ''
+    num1 = '0'
+    total = '0'
 }
 
-// console.log(add())
-// console.log(subtract())
-// console.log(multiply())
-// console.log(divide())
 
 let equals = document.querySelector('#equals')
-equals.onclick = function operate(num1, operator, newNum) {
-    console.log(add())
+equals.onclick = function operate() {
+    if (operator == '+') {
+        total = add()
+        startingNum.textContent = total
+        console.log(num1,operator,newNum + " = " + total)
+    } else if (operator == '-') {
+        total = subtract()
+        startingNum.textContent = total
+        console.log(num1,operator,newNum + " = " + total)
+    } else if (operator == '÷') {
+        total = divide()
+        startingNum.textContent = total
+        console.log(num1,operator,newNum + " = " + total)
+    } else if (operator == 'x') {
+        total = multiply()
+        startingNum.textContent = total
+        console.log(num1,operator,newNum + " = " + total)
+    }
+    num1 = String(total)
 }

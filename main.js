ef35b709d6e21c05c78e81b2b7402c2c9777c6ce
Guillermo Santiago <g.santiago3rd@gmail.com>
2024-02-02 13:20:00 -0700

    added decimal points and backspace

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 let startingNum = document.querySelector('#display');
 
-let newNum = '0'
+let newNum;
 
 let num1;
 let operator = '';
@@ -10,43 +10,63 @@ let total = 0;
 let digits = document.querySelectorAll(".digits button");
 digits.forEach(button => {
     button.onclick = function() {
-        if (newNum === '0' && button.innerText !== '0') {
+        if (newNum === undefined && button.innerText !== undefined) {
             newNum = button.innerText;
         } else {
-            newNum += button.innerText;
+            //Prevents multiple decimal points 
+            if (button.innerText === '.' && newNum.includes('.')) {
+                return;
+            }
+            newNum += button.innerText
         }
         startingNum.innerText = newNum
-        console.log(num1,newNum)
     };
 });
+
+let backspace = document.querySelector("#backspace");
+backspace.onclick = function() {
+    let currentText = startingNum.textContent;
+    let updatedText = currentText.slice(0, -1);
+    startingNum.textContent = updatedText;
+    newNum = updatedText;
+    // if (operator != '') {
+    //     newNum = undefined
+    // }
+    console.log(startingNum.textContent, newNum)
+}
 
 function add() {
     return Number(num1) + Number(newNum)
 }
-
 function subtract() {
     return Number(num1) - Number(newNum)
 }
-
 function multiply() {
     return Number(num1) * Number(newNum)
 }
-
 function divide() {
     return Number(num1) / Number(newNum)
 }
 
-clear = document.querySelector('#clear');
-clear.onclick = function() {
+function clear() {
     startingNum.textContent = '0';
-    newNum = '0';
+    newNum = undefined;
     operator = '';
     num1 = '0';
     total = '0';
 }
 
+let clearScrn = document.querySelector('#clear');
+clearScrn.onclick = clear
+
 function operate() {
-    if (operator == '+') {
+    if (newNum === undefined){
+        total = num1;
+        startingNum.textContent = total;
+    } else if ( operator == '÷' && newNum == '0'){
+        startingNum.textContent = "Stop that";
+        clear;
+    } else if (operator == '+') {
         total = add();
         startingNum.textContent = total;
         console.log(num1,operator,newNum + " = " + total);
@@ -64,7 +84,6 @@ function operate() {
         console.log(num1,operator,newNum + " = " + total);
     }
     num1 = String(total);
-    console.log(num1,newNum);
 
 }
 
@@ -78,10 +97,8 @@ operators.forEach(button => {
             num1 = newNum;
         } else if (operator != '') {
             operate();
-        }
+        } 
         operator = button.innerText;
-        newNum = '0';
-        // operate();
+        newNum = undefined;
     }
 });
-// operators.onclick = operate;
